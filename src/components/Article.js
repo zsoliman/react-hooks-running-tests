ef2e09e85fb04ba4c2b3d72d6ec6df0d285b0ee7
Zachary Soliman <zachsoliman1@gmail.com>
2022-07-19 10:34:26 -0400

    complete

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,9 +3,18 @@ import React from "react";
 function Article() {
   return (
     <div>
-      {/* please pass this test */}
+      please pass this test
     </div>
   );
 }
+
+test("displays the text 'please pass this test'", () => {
+  render(<Article />);
+
+  // add this line
+  screen.debug();
+
+  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+});
 
 export default Article;
